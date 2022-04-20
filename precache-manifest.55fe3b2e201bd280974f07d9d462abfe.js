ea9de3157a7aaeb453ff369bc797098c8137fea2
Prashant Gupta <prashantg4522@gmail.com>
2022-04-20 20:26:50 +0530

    Updates

---
+++ precache-manifest.55fe3b2e201bd280974f07d9d462abfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "931110f2dd387b08833c",
+    "url": "/Minesweeper/static/css/main.06510e20.chunk.css"
+  },
+  {
+    "revision": "931110f2dd387b08833c",
+    "url": "/Minesweeper/static/js/main.46277689.chunk.js"
+  },
+  {
+    "revision": "35a70fe54a2dfa1f8c3b",
+    "url": "/Minesweeper/static/js/runtime~main.427cc3a7.js"
+  },
+  {
+    "revision": "05f52691c656b122d4af",
+    "url": "/Minesweeper/static/js/2.282333d1.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/Minesweeper/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "726a07639579b2670309ac3e2a5b7906",
+    "url": "/Minesweeper/index.html"
+  }
+];
